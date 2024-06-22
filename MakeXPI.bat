81bf9518a5296d7c2030efa6a08db69781cf8471
Mike Irvin <irvinm@yahoo.com>
2024-06-22 17:14:58 -0400

    Create MakeXPI.bat

---
+++ MakeXPI.bat
@@ -0,0 +1,4 @@
+del TST-Active-and-Total-Tabs-Counter.xpi
+"C:\Program Files\WinRAR\winrar.exe" a  TST-Active-and-Total-Tabs-Counter.zip background.js manifest.json
+"C:\Program Files\WinRAR\winrar.exe" a -r TST-Active-and-Total-Tabs-Counter.zip images
+ren TST-Active-and-Total-Tabs-Counter.zip TST-Active-and-Total-Tabs-Counter.xpi
