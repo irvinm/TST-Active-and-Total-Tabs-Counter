6c8d231042e38b0430bcfb807eb59558d3e30c53
Mike Irvin <irvinm@yahoo.com>
2024-07-27 14:30:06 -0400

    Add code to handle display options from addon badge

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', async function() {
     const options = {
         switchToSVG: ['./images/BadgeText-9-Cropped.png', './images/BadgeText-99-Cropped.png', './images/BadgeText-999-Cropped.png', './images/SVG-1000-Cropped.png'],
-        alwaysSVG: ['./images/SVG-9-Cropped.png', './images/SVG-99-Cropped.png', './images/SVG-999-Cropped.png', './images/SVG-1000-Cropped.png']
+        alwaysSVG: ['./images/SVG-9-Cropped.png', './images/SVG-99-Cropped.png', './images/SVG-999-Cropped.png', './images/SVG-1000-Cropped.png'],
+        oneLinePerWindow: ['./images/OneLinePerWindow.png'],
+        compactView: ['./images/CompactView.png']
     };
 
     function updateImageRow(selectedOption) {
@@ -32,6 +34,27 @@ document.addEventListener('DOMContentLoaded', async function() {
         });
     }
 
+    function updateImageRow2(selectedOption) {
+        const imageRow = document.querySelector('#image-row2');
+        // Apply flexbox styles to center children horizontally
+        imageRow.style.display = 'flex';
+        imageRow.style.justifyContent = 'center';
+        imageRow.style.flexWrap = 'wrap'; // Optional, based on your layout needs
+
+        const images = options[selectedOption];
+
+        while (imageRow.firstChild) {
+            imageRow.removeChild(imageRow.firstChild);
+        }
+
+        images.forEach((src, index) => {
+            const img = document.createElement('img');
+            img.src = src;
+            img.alt = '';
+            imageRow.appendChild(img);
+        });
+    }
+
     try {
         let result = await browser.storage.local.get(['displayOption']);
         if (result.displayOption && options[result.displayOption]) {
@@ -42,6 +65,19 @@ document.addEventListener('DOMContentLoaded', async function() {
             document.getElementById("switchToSVG").checked = true;
             updateImageRow("switchToSVG");
         }
+        
+        let result2 = await browser.storage.local.get(['displayStyleOption']);
+        if (result2.displayStyleOption === "compactView") {
+            document.getElementById(result2.displayStyleOption).checked = true;
+            updateImageRow2(result2.displayStyleOption);
+            browser.runtime.sendMessage({ action: "setTabCountMethod", tabCountMethod: 2 });
+        } else {
+            await browser.storage.local.set({displayStyleOption: "oneLinePerWindow"});
+            document.getElementById("oneLinePerWindow").checked = true;
+            updateImageRow2("oneLinePerWindow");
+            browser.runtime.sendMessage({ action: "setTabCountMethod", tabCountMethod: 1 });
+        }
+
     } catch (error) {
         console.error('Error loading or setting default options:', error);
     }
@@ -51,19 +87,48 @@ document.addEventListener('DOMContentLoaded', async function() {
             if (this.checked) {
                 try {
                     await browser.storage.local.set({displayOption: this.id});
-                    console.log('Display option saved:', this.id);
+                    console.log('Badge counter option saved:', this.id);
                     updateImageRow(this.id);
 
                     const messageDiv = document.getElementById('message');
-                    messageDiv.textContent = 'User preference saved!';
+                    messageDiv.textContent = 'Badge counter option saved!';
                     messageDiv.style.display = 'block';
                     
                     setTimeout(() => {
                         messageDiv.textContent = '';
-                    }, 2000);
+                    }, 3000);
 
                     let response = await browser.runtime.sendMessage({action: "updateBadge"});
                     console.log("Badge update requested", response);
+                } catch (error) {
+                    console.error('Error saving display option or updating badge:', error);
+                }
+            }
+        });
+    });
+
+    document.querySelectorAll('input[name="displayStyleOption"]').forEach(radio => {
+        radio.addEventListener('change', async function() {
+            if (this.checked) {
+                try {
+                    await browser.storage.local.set({displayStyleOption: this.id});
+                    console.log('Display option saved:', this.id);
+                    updateImageRow2(this.id);
+
+                    const messageDiv = document.getElementById('message');
+                    messageDiv.textContent = 'Display option saved!';
+                    messageDiv.style.display = 'block';
+                    
+                    setTimeout(() => {
+                        messageDiv.textContent = '';
+                    }, 3000);
+
+                    if (this.id === "oneLinePerWindow") {
+                        browser.runtime.sendMessage({ action: "setTabCountMethod", tabCountMethod: 1 });
+                    } else {
+                        browser.runtime.sendMessage({ action: "setTabCountMethod", tabCountMethod: 2 });
+                    }
+
                 } catch (error) {
                     console.error('Error saving display option or updating badge:', error);
                 }
